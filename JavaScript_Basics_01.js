54359fe8598042a6996a488072aab49be2b5489b
Yuvaraj-B0 <byuvaraj979@gamil.com>
2025-04-28 23:40:38 +0530

    First_JS_file

---
+++ JavaScript_Basics_01.js
@@ -0,0 +1,3 @@
+console.log("Hello, world!"); //Print output in console page
+console.log("I'm JavaScript!"); //Print output in console page
+alert("Hello, world!,I'm JavaScript! "); //Print output in the webpage alert 
